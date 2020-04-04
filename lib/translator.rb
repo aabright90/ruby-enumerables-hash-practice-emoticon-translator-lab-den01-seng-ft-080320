d11db143d87a5670702dd21615322ac9a9194721
Austen Bright <pureaustenbright@gmail.com>
2020-04-04 04:26:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path, emoticon)
     # binding.pry
     if emoticon != emoticons[:english]
       "Sorry, that emoticon was not found"
-    elsif emoticon != emoticons[:english]
+    elsif emoticon == emoticons[:english]
       return emoticons[:japanese]
     end
   end
