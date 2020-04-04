1b0aa26813f5e3bd3811bc3085f286a02cb2c54c
Austen Bright <pureaustenbright@gmail.com>
2020-04-04 04:10:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,8 @@ def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   load_library(file_path)
   load_library(file_path).each do |meaning, emoticons|
-
+    binding.pry
+    if emoticon
   end
 end
 
