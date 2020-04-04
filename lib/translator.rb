7a67c7d4a03580ef87d57a6558af5fd3edbba18a
Austen Bright <pureaustenbright@gmail.com>
2020-04-04 03:50:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,8 +20,8 @@ def get_japanese_emoticon(file_path)
   # code goes here
   load_library(file_path)
   load_library(file_path).each do |key, value|
+    binding.pry
   end
-  binding.pry
 end
 
 def get_english_meaning
