5d39d4850372a673c4cb8d75f461e64e286cdf53
Austen Bright <pureaustenbright@gmail.com>
2020-04-04 04:32:42 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,14 +16,28 @@ def load_library(file_path)
 new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   load_library(file_path)
-  load_library(file_path).each do |key, value|
-    binding.pry
+  load_library(file_path).each do |meaning, emoticons|
+    # binding.pry
+    if emoticon == emoticons[:english]
+      return emoticons[:japanese]
+    else
+    end
   end
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
+  # load_library(file_path)
+  load_library(file_path).each do |meaning, emoticons|
+    # binding.pry
+    if emoticon == emoticons[:japanese]
+      return meaning
+    else
+    end
+  end
+  return "Sorry, that emoticon was not found"
 end
